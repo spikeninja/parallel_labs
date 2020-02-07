7610b3994a5600d3445f9339a5cff57a77ac9625
ArturSamvelyan <yourcamelcase@gmail.com>
2020-02-07 07:07:15 +0200

    Files prepared to parallel_addition

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 const int N = 10;
 const int M = 10;
 
-const int NUMBER_THREADS = 1;
+const int NUM_OF_THREADS = 1;
 
 
 int main(){
@@ -24,10 +24,23 @@ int main(){
   auto delta = chrono::duration_cast<chrono::microseconds>(t2_seq - t1_seq).count();
   cout << "Sequential adding: " << delta << " microsecond" << endl; 
 
-  //cout << a;
+  // Parallel addition
 
-  //thread threads[NUMBER_THREADS];
+  vector<thread> threads;
+  Matrix d(N, M);
+  chrono::time_point <chrono::system_clock> t1_par = chrono::high_resolution_clock::now();
 
+  for(int i = 0; i < NUM_OF_THREADS; i++){
+    threads.push_back(thread(d.parallel_addition,d,a,b,i * (N / NUM_OF_THREADS),(i + 1) * (N / NUM_OF_THREADS)));
+  }
+
+  for(auto &thread : threads){
+    thread.join();
+  }
+
+  chrono::time_point <chrono::system_clock> t2_par = chrono::high_resolution_clock::now();
+  auto duration_par = std::chrono::duration_cast<std::chrono::microseconds>(t2_par - t1_par).count();
+  std::cout << "Parallel (" << NUM_OF_THREADS << " threads) addition of vectors - execution time: " << duration_par << "us" << std::endl;
    
 
   return 0;
