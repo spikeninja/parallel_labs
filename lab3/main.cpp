a9a813c046e90f12776c4a00e93c4ba88ed4a35b
ArturSamvelyan <yourcamelcase@gmail.com>
2020-03-15 15:35:19 +0200

    Lab3 initial commit

---
+++ lab3/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <chrono>
+#include <mutex>
+#include <unistd.h>
+
+using namespace std;
+
+
+int main(){
+  srand(time(NULL));
+ 
+  return 0;
+}
