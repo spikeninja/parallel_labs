63f38561de026e8d337c66cf307e9c6bbcd94028
ArturSamvelyan <yourcamelcase@gmail.com>
2020-04-01 17:04:58 +0300

    compare_exchange added, works

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -5,6 +5,7 @@
 #include <mutex>
 #include <unistd.h>
 #include <atomic>
+#include <memory>
 
 using namespace std;
 
@@ -24,8 +25,10 @@ using namespace std;
 //long multiSum = 0;
 atomic<long> multiSum(0);
 
-void vec_size_t(const vector<int>& vec, int idxStart, int idxStop){
-
+void vec_size_t(const vector<int>& vec, atomic<int>& vec_size, int idxStart, int idxStop){
+  for(int i = idxStart; i <= idxStop; i++){
+    vec_size++;
+  }
 }
 
 void max_elem_t(const vector<int>& vec, atomic<int>& max, int idxStart, int idxStop){
@@ -36,9 +39,24 @@ void max_elem_t(const vector<int>& vec, atomic<int>& max, int idxStart, int idxS
   }
 }
 
-void vec_size(const vector<int>& vec, atomic<int>& arr_size){
-
+void min_elem_t(vector<int>& vec, atomic<int>& min, int idxStart, int idxStop){
+  for(int i = idxStart; i <= idxStop; i++){
+    if(vec[i] < min){
+      min.compare_exchange_strong(vec[i], min);
+    }
+  }
 }
+
+void vec_size(const vector<int>& vec, atomic<int>& vec_size){
+  thread threads[3];
+  threads[0] = thread(vec_size_t, ref(vec), ref(vec_size), 0, vec.size()/3);
+  threads[1] = thread(vec_size_t, ref(vec), ref(vec_size), vec.size()/3, 2*vec.size()/3);
+  threads[2] = thread(vec_size_t, ref(vec), ref(vec_size), 2*vec.size()/3, vec.size());
+  for(int i = 0; i < 3; i++){
+    threads[i].join();
+  }
+}
+
 void max_elem(const vector<int>& vec, atomic<int>& max){
   max = vec[0];
   thread threads[3];
@@ -49,9 +67,17 @@ void max_elem(const vector<int>& vec, atomic<int>& max){
     threads[i].join();
   }
 }
-void min_elem(const vector<int>& vec){
-
+void min_elem(vector<int>& vec, atomic<int>& min){
+  min = vec[0];
+  thread threads[3];
+  threads[0] = thread(min_elem_t, ref(vec), ref(min), 0, vec.size()/3);
+  threads[1] = thread(min_elem_t, ref(vec), ref(min), vec.size()/3, 2*vec.size()/3);
+  threads[2] = thread(min_elem_t, ref(vec), ref(min), 2*vec.size()/3, vec.size());
+  for(int i = 0; i < 3; i++){
+    threads[i].join();
+  }
 }
+
 void sum_numbers(const vector<int>& vec, int idxStart, int idxEnd){
   for(int i = idxStart; i <= idxEnd; i++){
     multiSum += vec[i];
@@ -64,13 +90,18 @@ int main(){
   vector<int> vec;
   atomic<int> arr_size, max, min, xor_num;
 
-  for(int i = 0; i < 3000; i++){
-    vec.push_back(rand()%1000);
+  for(int i = 0; i < 10; i++){
+    vec.push_back(rand()%1000+23);
   }
 
   max_elem(vec, max);
+  min_elem(vec, min);
 
-  cout << max << endl;
+  for(int i = 0; i < vec.size(); i++){
+    cout << vec[i] << " ";
+  }
+  cout << endl;
+  cout << max << " " << min << endl;
 
   return 0;
 }
