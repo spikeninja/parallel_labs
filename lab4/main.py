190432974003eb8b58f5189f2ce119b8d005ed33
ArturSamvelyan <yourcamelcase@gmail.com>
2020-04-18 14:23:49 +0300

    ready lab4

--- lab4/main.py
+++ lab4/main.py
@@ -1,46 +1,47 @@
 from random import randint
 from time import time
+import asyncio
 
-def randlist(amount):
-    res = []
-    for i in range(amount):
-        res.append(randint(1, 100))
-    return res
-
-def multiply_by(lst, num):
+async def multiply_by(lst, num):
     print(f"Multiplying by {num}...")
-    res = list(map(lambda x: x*num, lst))
+    res = []
+    for i in lst:
+        res.append(i*num)
     return sorted(res)
 
-def leave_even(lst):
+async def leave_even(lst):
     print(f"Deleting odds...")
-    res = list(filter(lambda x: x % 2 == 0, lst))
+    res = []
+    for i in range(len(lst)):
+        if i % 2 == 0:
+            res.append(i)
     return sorted(res)
 
-def leave_case(lst):
+async def leave_case(lst):
     _max = max(lst)
     print(f"Leaving in the interval: {0.4*_max}<=x<= {0.6*_max}...")
-    res = list(filter(lambda x: 0.4*_max <= x <= 0.6*_max, lst))
+    res = []
+    for i in range(len(lst)):
+        if 0.4*_max < i < 0.6*_max:
+            res.append(i)
     return sorted(res)
+
 
 def _main():
     AMOUNT = 200000
-    start = time()
     lists = [randlist(AMOUNT) for i in range(3)]
-    #print("List 1:", lists[0])
-    #print("List 2:", lists[1])
-    #print("List 3:", lists[2])
 
-    lmulti = multiply_by(lists[0], 5)
-    #print(lmulti)
-    leven = leave_even(lists[1])
-    #print(leven)
-    lcase = leave_case(lists[2])
-    #print(lcase)
+    functions = [
+        asyncio.ensure_future(multiply_by(lists[0], 5)),
+        asyncio.ensure_future(leave_even(lists[1])),
+        asyncio.ensure_future(leave_case(lists[2])),
+    ]
+
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(asyncio.gather(*functions))
+    event_loop.close()
+
     result = lmulti + leven + lcase
-    stop = time()
-    duration = stop - start
-    print("Duration: ", duration)
 
 if __name__ == '__main__':
     _main()
