0edb9f446615823faf35aa6fd32c9bd432b8c1f9
ArturSamvelyan <yourcamelcase@gmail.com>
2020-04-19 12:44:20 +0300

    all works without describing data in other client

--- lab5/client.py
+++ lab5/client.py
@@ -1,1 +1,54 @@
 import socket
+import threading
+import sys
+
+def recv_data():
+    "Receive data from other clients connected to server"
+    while True:
+        try:
+            recv_data = client_socket.recv(4096)
+        except:
+            #Handle the case when server process terminates
+            print("Server closed connection, thread exiting.")
+            #_thread.interrupt_main()
+            break
+        if not recv_data:
+                # Recv with no data, server closed connection
+                print("Server closed connection, thread exiting.")
+                #_thread.interrupt_main()
+                break
+        else:
+                print("Received data: "), recv_data
+
+def send_data():
+    while True:
+        send_data = str(input("Enter data to send (q or Q to quit):"))
+        if send_data == "q" or send_data == "Q":
+            client_socket.send(send_data.encode("utf8"))
+            #_thread.interrupt_main()
+            break
+        else:
+            client_socket.send(send_data.encode("utf8"))
+
+if __name__ == "__main__":
+
+    print("========TCP/IP Chat client program========")
+    print("Connecting to server at 127.0.0.1:5000")
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('127.0.0.1', 5000))
+
+    print("Connected to server at 127.0.0.1:5000")
+
+    th1 = threading.Thread(target=recv_data,args=())
+    th2 = threading.Thread(target=send_data,args=())
+
+    th1.start()
+    th2.start()
+
+    try:
+        while True:
+            continue
+    except:
+        print("Client program quits....")
+        client_socket.close()
--- lab5/server.py
+++ lab5/server.py
@@ -1,1 +1,60 @@
 import socket
+import select
+import string
+
+def broadcast_data (sock, message, sockets, server_socket):
+# відсилає повідомлення message усім клієнтам приєднаним до чата
+    for socket in sockets:
+        if socket != server_socket and socket != sock:
+            socket.send(message)
+
+
+def main():
+    CONNECTION_LIST=[]
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("127.0.0.1", 5000))
+    server_socket.listen()
+
+    CONNECTION_LIST.append(server_socket)
+
+    print("TCP/IP Chat server.")
+
+    while True:
+        read_sockets, write_sockets, error_sockets = select.select(CONNECTION_LIST,[],[])
+
+        for sock in read_sockets:
+
+            if sock == server_socket:
+                # Handle the case in which there is a new connection recieved
+                # through server_socket
+                sockfd, addr = server_socket.accept()
+                CONNECTION_LIST.append(sockfd)
+                print(f"Client ({addr}, {addr}) connected")
+                broadcast_data(sockfd, f"Client ({addr}, {addr}) connected".encode("utf8"), CONNECTION_LIST, server_socket)
+
+            else:
+                # дані отримані від клієнтів (їх треба обробити)
+                try:
+                    data = sock.recv(4096)
+                except:
+                    broadcast_data(sock, f"Client ({addr}, {addr}) is offline".encode("utf8"), CONNECTION_LIST, server_socket)
+                    print(f"Client ({addr}, {addr}) is offline")
+                    sock.close()
+                    CONNECTION_LIST.remove(sock)
+                    continue
+
+                if data:
+                    # The client sends some valid data, process it
+                    if data == "q" or data == "Q":
+                        broadcast_data(sock, f"Client ({addr}, {addr}) quits".encode("utf8"), CONNECTION_LIST, server_socket)
+                        print(f"Client ({addr}, {addr}) quits")
+                        sock.close()
+                        CONNECTION_LIST.remove(sock)
+                    else:
+                        broadcast_data(sock, data, CONNECTION_LIST, server_socket)
+
+    server_socket.close()
+
+if __name__ == "__main__":
+    main()
